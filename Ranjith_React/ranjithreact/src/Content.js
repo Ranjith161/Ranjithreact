66b9ef0c68036b8f8d818cec98144a5ca1afbef8
SASIKUMAR RAMASAMY <ranjithmurugan18@gmail.com>
2023-09-21 22:08:48 +0530

    done react

--- Ranjith_React/ranjithreact/src/Content.js
+++ Ranjith_React/ranjithreact/src/Content.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useState } from 'react'
 
 const Content = () => {
-    function handleWords(){
-        const words=["am","am not","was"]
-        const int=Math.floor(Math.random()*3)
-        return words[int]
-      }
+  const[items,setItems]= useState([{id:1,checked:true,item:"Play cricket"},
+  {id:1,checked:true,item:"footbal"},
+  {id:1,checked:true,item:"coco"}])
+  
+      
+     
   return (
-    <div>I {handleWords() } here.</div>
+    <main>
+      <ul>
+        {items.map((item)=>(
+          <li>
+            <input type="checkbox"
+            checked={item.checked} />
+            <label >{item.item}</label>
+            <button>delete</button>
+          </li>
+        ))}
+      </ul>
+    </main>
   )
 }
 
